26de9bdc4806fad2c7bb32ff66dd478d8dd82749
Joel Fox Apelheed <joel.apelheed@noaignite.com>
2025-02-19 15:17:52 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './Gammelfernet.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          GammelFernet
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
