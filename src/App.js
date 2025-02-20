510295befcf9db2d52569bd547f6c537a4f53314
Joel Fox Apelheed <joel.apelheed@noaignite.com>
2025-02-20 11:22:59 +0100

    ADDED IMAGE OF GAMMELFERNET TO START PAGE

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://gammelfernet.se/gammelfernet_oskars.jpg" className="App-logo" alt="logo" />
         <h1>
           GammelFernet
         </h1>
